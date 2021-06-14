c992ed172c2c3fb3d77ef4da053e73f1a7ab4d4f
J Mochizuki <junya00mochizuki@gmail.com>
2021-06-15 07:42:17 +0900

    Add templates components pages gatsby-nodes.js

---
+++ src/pages/blog.js
@@ -0,0 +1,38 @@
+import React from'react'
+import { graphql, Link } from 'gatsby'
+import Layout from '../components/layout'
+
+export const query = graphql`
+    query {
+        wpgraphql {
+            posts {
+                nodes {
+                    id
+                    title
+                    uri
+                    excerpt
+                }
+            }
+        }
+    }
+`
+
+const Blog = ({ data }) => {
+    const posts = data.wpgraphql.posts.nodes
+
+    return (
+        <Layout>
+            {posts.map(post => (
+                <article  key={post.id}>
+                    <h2>
+                        <Link to={`/blog${post.uri}`} dangerouslySetInnerHTML={{ __html: post.title }} />
+                        <div dangerouslySetInnerHTML={{ __html: post.excerpt}} />
+                    </h2>
+
+                </article>
+            ))}
+        </Layout>
+    )
+}
+
+export default Blog
