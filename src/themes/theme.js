72c888d5cc70a3f523aae27c825b08362852154b
J Mochizuki <junya00mochizuki@gmail.com>
2021-06-14 14:58:57 +0900

    Add styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,29 @@
+export default {
+  fonts: {
+    main: "Muli, sans-serif",
+    code: "Noto Sans JP, sans-serif",
+  },
+  colors: {
+    main1: "hsl(207,70%,59%)",
+  },
+  breakpoints: {
+    pc: "only screen and (min-width: 1025px)",
+    tablet: "only screen and (min-width: 600px)",
+  },
+  spacing: {
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xlarge: "4rem",
+    xxlarge: "6rem",
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 0px 15px rgba(0, 0, 0, 0.1)",
+  },
+}
